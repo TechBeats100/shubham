609f68f9432bc0c77d1b629fe449ee60078f5543
TechBeats100 <32466171+TechBeats100@users.noreply.github.com>
2017-10-29 01:32:22 -0400

    changes in both

--- shubham.c
+++ shubham.c
@@ -3,8 +3,9 @@ void main()
 {
 int c;
 for(c=1;c<=10;c++)
-{printf("Hello this is Shubham");
- printf("This is my first assignment");
- printf("All is going great");
+{printf("Hello this is Shubham\n");
+ printf("This is my first assignment\n");
+ printf("All is going great\n");
+ printf("Hope you are enjoying\n");
 }
 }
